0ffb00c2eb8dfcf8d8e8207ed4f70e0fdc614e68
mcdan <mcweeney@adobe.com>
2015-04-16 17:01:16 -0400

    Simple Solr Cloud configs are working.

--- test/simple-cloud-test.js
+++ test/simple-cloud-test.js
@@ -7,10 +7,13 @@ var mocha = require('mocha'),
 	assert = require('chai').assert,
 	libPath = process.env['SOLR_CLIENT_COV'] ? '../lib-cov' : '../lib',
 	solr = require( libPath + '/solr'),
-	sassert = require('./sassert');
+	sassert = require('./sassert'),
+	argv = require('minimist')(process.argv.slice(2));
 
 // Test suite
-var config = figc(__dirname + '/cloud-config.json');
+var configPath = argv.configPath || 'config.json';
+console.log("CONFIG PATH: %s", configPath);
+var config = figc(__dirname + '/' + configPath);
 var client = solr.createClient(config.client);
 var basePath = [config.client.path, config.client.core].join('/').replace(/\/$/,"");
 
